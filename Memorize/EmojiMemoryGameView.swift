5f7f1c96b58b1e311397effd1e571efa5edf8cab
Ram Jondhale <ramjondhale1@gmail.com>
2023-07-06 22:02:03 +0530

    - Added support to adjust size of icon based on card size using GeometryReader - Done some refactoring

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -61,18 +61,30 @@ struct CardView: View {
     let card: EmojiMemoryGame.Card
 
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content).font(.largeTitle)
-            } else if card.isMatched {
-                shape.opacity(0)
-            } else {
-                shape.fill()
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstant.cornerRadius)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: DrawingConstant.lineWidth)
+                    Text(card.content).font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else {
+                    shape.fill()
+                }
             }
         }
+    }
+
+    private func font(in size: CGSize) -> Font {
+        return .system(size: min(size.width, size.height) * DrawingConstant.fontScale)
+    }
+
+    struct DrawingConstant {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
     }
 }
 
